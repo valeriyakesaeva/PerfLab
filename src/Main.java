5fc291d28863a11a96f855c83bdb31606b79f7f2
Кесаева Валерия <valeriya.kesaeva@gmail.com>
2026-03-14 17:20:08 +0300

    Homework1.1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int b = 1;
+        int c = 1;
+        int a = 4*(b+c-1)/2;
+        System.out.println(a);
+
+    }
+}
